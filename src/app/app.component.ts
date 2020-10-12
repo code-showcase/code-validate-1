3a6aa8d6ac6f881c4bfeecc7c6bf8408996d24cc
code-showcase <deepti.barhi@gmail.com>
2020-10-12 11:09:39 +0530

    project commit 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CapitalizeFirstPipe } from './capitalize.pipe';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers: [CapitalizeFirstPipe]
+})
+
+export class AppComponent {
+  title = 'angular form Validation Tutorial';
+  angForm: FormGroup;
+  constructor(private fb: FormBuilder, public capitalize: CapitalizeFirstPipe) {
+    this.createForm();
+  }
+  createForm() {
+    this.angForm = this.fb.group({
+      fname: ['', Validators.required],
+      lname: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.title = this.capitalize.transform(this.title);
+
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CapitalizeFirstPipe } from './capitalize.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CapitalizeFirstPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
